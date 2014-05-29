914eaffd4a36979b4c844c5a736013c750bf7efc
Nicolas Assaleh <nick.assaleh@gmail.com>
2014-05-29 11:59:42 -0400

    Created Student.cs and Belt.cs classes

---
+++ KarateManagement/KarateManagement/Belt.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KarateManagement
+{
+    public enum Belt
+    {
+        White = 0,
+        Orange = 1,
+        Blue = 2,
+        Yellow = 3,
+        Green = 4,
+        Brown = 5,
+        Black = 6,
+        Nidan = 7,
+        Sandan = 8,
+        Yodan = 9,
+        Godan = 10
+    }
+}
---
+++ KarateManagement/KarateManagement/Student.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KarateManagement
+{
+    /// <summary>
+    /// This class will be a representation of a student
+    /// </summary>
+    class Student
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+
+        public string Address { get; set; }
+
+        public string PostalCode { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public string Email { get; set; }
+
+        public int Hours { get; set; }
+
+        public Belt Belt { get; set; }
+
+
+
+
+    }
+}
